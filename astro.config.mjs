ff870b2eafd28d23d48b86e8674fafc8854dee9f
Name <you@example.com>
2026-06-16 11:44:33 +0800

    feat: 添加推荐工具 issue 模板

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,10 @@ import starlight from "@astrojs/starlight";
 export default defineConfig({
   integrations: [
     starlight({
-      title: "Bookmarks",
+      title: "闪光小破站",
       defaultLocale: "root",
       locales: { root: { label: "中文", lang: "zh-CN" } },
-      sidebar: [
-        { label: "书签", slug: "bookmarks" },
-      ],
+      sidebar: [{ label: "书签", slug: "bookmarks" }],
       customCss: ["./src/styles/custom.css"],
     }),
   ],
